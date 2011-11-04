22cf70c2e11136b87b9fbf77a2e81220b0e43c40
Thomas Chen <foxnewsnetwork@gmail.com>
2011-11-03 21:47:57 -0700

    fixed up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 EmailGen::Application.routes.draw do
-  get "pages/home"
+  
 
   get "pages/about"
 
@@ -54,7 +54,7 @@ EmailGen::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
