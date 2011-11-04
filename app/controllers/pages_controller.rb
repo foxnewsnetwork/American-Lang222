60215dda41c40404d5c699a301a5b731a6a26666
Thomas Chen <foxnewsnetwork@gmail.com>
2011-11-04 12:18:12 -0700

    made it so that it now works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,23 @@
+require 'date'
+
 class PagesController < ApplicationController
   def home
-    unless params[:address].nil?
-      keywords = []
-      params[:address].each do |key value|
-        keywords.push(value)
+    @addresses = {}
+    unless params[:address].empty?
+      @s = params[:address]
+      @s[:dates] = @s[:dates].split(',')
+      unless @s[:dates].empty?
+        for k in 0..@s[:dates].count-1 do
+          @s[:dates][k] = Date.parse(@s[:dates][k])
+        end
       end
-      @addresses = GenerateBCCField(keywords)
+      @s[:domains] = @s[:domains].split(',')
+      @addresses = GenerateBCCField(@s)
+      unless @s[:firstname].empty? || @s[:lastname].empty?
+        for k in 0..(@addresses.count-1) do
+          @addresses[k] = "#{@s[:firstname].capitalize} #{@s[:lastname].capitalize} <#{@addresses[k]}>" 
+        end
+      end
     end
   end
 
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module PagesHelper
   
   # dataset is an hash of known information about a person
@@ -7,43 +9,41 @@ module PagesHelper
     
     # Set 0: generating initials permutation
     useset = []
-    useset += dataset[:firstname][0] unless dataset[:firstname].nil?
-    useset += dataset[:middlename][0] unless dataset[:middlename].nil?
-    useset += dataset[:lastname][0] unless dataset[:lastname].nil?
-    addresses += self.PermuteAll(useset) unless useset.nil?
+    useset += [dataset[:firstname][0]] unless dataset[:firstname].empty?
+    useset += [dataset[:middlename][0]] unless dataset[:middlename].empty?
+    useset += [dataset[:lastname][0]] unless dataset[:lastname].empty?
+    addresses += PermuteAll(useset) unless useset.empty?
     
     # Set 1: generating first-middle initials last name permutations
     useset = []
-    first_middle = []
-    first_middle += dataset[:firstname][0] unless dataset[:firstname].nil?
-    first_middle += dataset[:middlename][0] unless dataset[:middlename].nil?
-    useset += [first_middle] unless first_middle.nil?
-    useset += dataset[:lastname] unless dataset[:lastname].nil?
-    addresses += self.PermuteAll(useset) unless useset.nil?
+    first_middle = ""
+    first_middle += dataset[:firstname][0] unless dataset[:firstname].empty?
+    first_middle += dataset[:middlename][0] unless dataset[:middlename].empty?
+    useset += [first_middle] unless first_middle.empty?
+    useset.push(dataset[:lastname]) unless dataset[:lastname].empty?
+    addresses += PermuteAll(useset) unless useset.empty?
     
     # Set 2: generating middle-last initials first name permutations
     useset = []
-    middle_last = []
-    middle_last +=  dataset[:middlename][0] unless dataset[:middlename].nil?
-    middle_last += dataset[:lastname][0] unless dataset[:lastname].nil?
-    useset += [middle_last] unless middle_last.nil?
-    useset += dataset[:firstname] unless dataset[:firstname].nil?
-    addresses += self.PermuteAll(useset) unless useset.nil?
+    middle_last = ""
+    middle_last += dataset[:middlename][0] unless dataset[:middlename].empty?
+    middle_last += dataset[:lastname][0] unless dataset[:lastname].empty?
+    useset += [middle_last] unless middle_last.empty?
+    useset.push(dataset[:firstname]) unless dataset[:firstname].empty?
+    addresses += PermuteAll(useset) unless useset.empty?
     
     # Set 3: generating full name permutations
     useset = []
-    useset += dataset[:firstname] unless dataset[:firstname].nil?
-    useset += dataset[:middlename] unless dataset[:middlename].nil?
-    useset += dataset[:lastname] unless dataset[:lastname].nil?
-    addresses += self.PermuteAll(useset) unless useset.nil?
+    useset.push(dataset[:firstname]) unless dataset[:firstname].empty?
+    useset.push(dataset[:middlename]) unless dataset[:middlename].empty?
+    useset.push(dataset[:lastname]) unless dataset[:lastname].empty?
+    addresses += PermuteAll(useset) unless useset.empty?
     
     # Set 4: repeating steps 0-3 now with dates
     dates = []
-    dates = dataset[:dates] unless dataset[:dates].nil?
+    dates = dataset[:dates] unless dataset[:dates].empty?
     nominal_addresses = Array.new(addresses)
     dates.each do |date|
-      year = []
-      monthday = []
       year = "#{date.year}"[-2..-1]
       monthday = "#{date.month}#{date.day}"
       lolcat = Array.new(nominal_addresses)
@@ -56,7 +56,7 @@ module PagesHelper
     end
     
     # Set 5: attaching domains
-    domains = dataset[:domains].nil? ? [] : dataset[:domains]
+    domains = dataset[:domains].empty? ? [] : dataset[:domains]
     out_value = []
     domains.each do |domain|
       na = Array.new(addresses)
@@ -70,21 +70,20 @@ module PagesHelper
     return out_value[0..limit-1]
   end
   
-  private
-    # returns a string array of every single permutation of the elements in arr_in
-    # Necessarily returns an array of size n!
-    def self.PermuteAll( arr_in )
-      return arr_in if arr_in.count <= 1
-      
-      arr_out = []
-      for k in 0..arr_in.count-1 do
-        recurse = Array.new(arr_in)
-        recurse.delete_at(k)
-        temp = self.PermuteAll( recurse )
-        for j in 0..temp.count-1 do
-          arr_out += [arr_in[k] + temp[j]]
-        end
+  # returns a string array of every single permutation of the elements in arr_in
+  # Necessarily returns an array of size n!
+  def PermuteAll( arr_in )
+    return arr_in if arr_in.count <= 1
+    
+    arr_out = []
+    for k in 0..arr_in.count-1 do
+      recurse = Array.new(arr_in)
+      recurse.delete_at(k)
+      temp = PermuteAll( recurse )
+      for j in 0..temp.count-1 do
+        arr_out += [arr_in[k] + temp[j]]
       end
-      return arr_out
     end
+    return arr_out
+  end
 end
